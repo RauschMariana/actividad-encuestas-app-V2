68a56b92a82ba7d218651bfb1fc8e0a18b9c33d3
Mariana Rausch <marianarauschb@gmail.com>
2023-10-24 20:47:43 -0300

    first commit

---
+++ src/components/Inicio.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Inicio = ({ listaEncuestas }) => {
+    console.log( listaEncuestas );
+  return (
+    <>
+        <div>
+            <h1>Lista de Encuestas Disponibles</h1>
+            { listaEncuestas.map(( encuesta ) => (
+                <div className="encuesta-item-container" key={ encuesta.id }>
+                    <div className="encuesta-item">
+                        <h2>{ encuesta.titulo }</h2>
+                        <p>{ encuesta.descripcion }</p>
+                        <Link to={`/encuesta/${ encuesta.id }`}>Ver encuesta</Link>
+                        <br />
+                    </div>
+                </div> ))
+            }
+        </div>
+    </>
+  );
+};
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return (
+    <>
+      <div>
+        <h2>Página no encontrada</h2>
+        <p>Lo sentimos, la página que estás buscando no existe.</p>
+      </div>
+    </>
+  );
+};
+ 
